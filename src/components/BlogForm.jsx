fd698442ff2b45e1a335cfde14e60f840e3c7479
Daniel Silva <danielsmartins19@gmail.com>
2025-07-24 19:20:30 -0300

    Tests

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,29 +1,55 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react' 
 
 const BlogForm = ({ createBlog }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    const newBlog = {
-      title: event.target.title.value,
-      author: event.target.author.value,
-      url: event.target.url.value,
-    }
-    createBlog(newBlog)
+    // 4. Usar o estado no submit
+    createBlog({
+      title,
+      author,
+      url,
+    })
+
+    
+    setTitle('')
+    setAuthor('')
+    setUrl('')
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         Title:
-        <input name="title" placeholder="title" />
+       
+        <input
+          name="title"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+          placeholder="title"
+        />
       </div>
       <div>
         Author:
-        <input name="author" placeholder="author" />
+        <input
+          name="author"
+          value={author}
+          onChange={({ target }) => setAuthor(target.value)}
+          placeholder="author"
+        />
       </div>
       <div>
         URL:
-        <input name="url" placeholder="url" />
+        <input
+          name="url"
+          value={url}
+          onChange={({ target }) => setUrl(target.value)}
+          placeholder="url"
+        />
       </div>
       <button type="submit">create</button>
     </form>
@@ -31,7 +57,7 @@ const BlogForm = ({ createBlog }) => {
 }
 
 BlogForm.propTypes = {
-  createBlog: PropTypes.func.isRequired
+  createBlog: PropTypes.func.isRequired,
 }
 
 export default BlogForm
--- src/components/BlogForm.test.js
+++ src/components/BlogForm.test.js
@@ -1,4 +1,5 @@
-
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
